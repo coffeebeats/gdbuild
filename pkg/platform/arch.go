54699a3f3647c93dacf82c0f593c5daacb6e4311
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-02-12 22:42:21 -0800

    feat(platform): define OS and Arch enums for representing platform information

---
+++ pkg/platform/arch.go
@@ -0,0 +1,103 @@
+package platform
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var (
+	ErrMissingArch      = errors.New("missing architecture")
+	ErrUnrecognizedArch = errors.New("unrecognized architecture")
+)
+
+/* -------------------------------------------------------------------------- */
+/*                                 Enum: Arch                                 */
+/* -------------------------------------------------------------------------- */
+
+// CPU architectures which Godot supports targeting.
+type Arch uint
+
+const (
+	ArchAmd64 Arch = iota + 1
+	ArchArm32
+	ArchArm64
+	ArchI386
+	ArchUniversal
+)
+
+/* ----------------------------- Impl: Stringer ----------------------------- */
+
+// String implements 'fmt.Stringer' for 'Arch' according to the architecture
+// names passed to SCons during compilation.
+func (a Arch) String() string {
+	switch a {
+	case ArchAmd64:
+		return "x86_64"
+	case ArchArm32:
+		return "arm32"
+	case ArchArm64:
+		return "arm64"
+	case ArchI386:
+		return "x86_32"
+	case ArchUniversal:
+		return "universal"
+	default:
+		return ""
+	}
+}
+
+/* --------------------------- Function: ParseArch -------------------------- */
+
+// Parses an input string as a CPU architecture specification.
+//
+// NOTE: This is a best effort implementation. Please open an issue on GitHub
+// if some values are missing:
+// github.com/coffeebeats/gdbuild/issues/new?labels=bug&template=%F0%9F%90%9B-bug-report.md.
+func ParseArch(input string) (Arch, error) {
+	switch strings.ToLower(strings.TrimSpace(input)) {
+	case "":
+		return 0, ErrMissingArch
+
+	case "amd64", "x86_64", "x86-64":
+		return ArchAmd64, nil
+
+	case "arm64", "arm64be":
+		return ArchArm64, nil
+
+	case "386", "i386", "x86", "x86_32":
+		return ArchI386, nil
+
+	case "fat", "universal":
+		return ArchUniversal, nil
+
+	default:
+		return 0, fmt.Errorf("%w: '%s'", ErrUnrecognizedArch, input)
+	}
+}
+
+/* ------------------------- Function: MustParseArch ------------------------ */
+
+// Parses an input string as a CPU architecture specification but panics if it
+// would fail.
+func MustParseArch(input string) Arch {
+	arch, err := ParseArch(input)
+	if err != nil {
+		panic(err)
+	}
+
+	return arch
+}
+
+/* ---------------------- Impl: encoding.UnmarshalText ---------------------- */
+
+func (a *Arch) UnmarshalText(bb []byte) error {
+	value, err := ParseArch(string(bb))
+	if err != nil {
+		return err
+	}
+
+	*a = value
+
+	return nil
+}
---
+++ pkg/platform/os.go
@@ -0,0 +1,112 @@
+package platform
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var (
+	ErrMissingOS      = errors.New("missing OS")
+	ErrUnrecognizedOS = errors.New("unrecognized OS")
+)
+
+/* -------------------------------------------------------------------------- */
+/*                                  Enum: OS                                  */
+/* -------------------------------------------------------------------------- */
+
+// Operating systems which the Godot project supports building for.
+type OS int
+
+const (
+	OSAndroid OS = iota + 1
+	OSIOS
+	OSLinux
+	OSMacOS
+	OSWeb
+	OSWindows
+)
+
+/* ----------------------------- Impl: Stringer ----------------------------- */
+
+// String implements 'fmt.Stringer' for 'OS' according to the platform names
+// passed to SCons during compilation.
+func (o OS) String() string {
+	switch o {
+	case OSAndroid:
+		return "android"
+	case OSIOS:
+		return "ios"
+	case OSLinux:
+		return "linuxbsd"
+	case OSMacOS:
+		return "macos"
+	case OSWeb:
+		return "web"
+	case OSWindows:
+		return "windows"
+	default:
+		return ""
+	}
+}
+
+/* ---------------------------- Function: ParseOS --------------------------- */
+
+// Parses an input string as an operating system specification.
+//
+// NOTE: This is a best effort implementation. Please open an issue on GitHub
+// if some values are missing:
+// github.com/coffeebeats/gdbuild/issues/new?labels=bug&template=%F0%9F%90%9B-bug-report.md.
+func ParseOS(input string) (OS, error) {
+	switch strings.ToLower(strings.TrimSpace(input)) {
+	case "":
+		return 0, ErrMissingOS
+
+	case "android":
+		return OSAndroid, nil
+
+	case "ios":
+		return OSIOS, nil
+
+	case "linux", "linuxbsd", "x11":
+		return OSLinux, nil
+
+	case "darwin", "macos", "osx":
+		return OSMacOS, nil
+
+	case "web":
+		return OSWeb, nil
+
+	case "win", "windows":
+		return OSWindows, nil
+
+	default:
+		return 0, fmt.Errorf("%w: '%s'", ErrUnrecognizedOS, input)
+	}
+}
+
+/* -------------------------- Function: MustParseOS ------------------------- */
+
+// Parses an input string as an operating system specification but panics if it
+// would fail.
+func MustParseOS(input string) OS {
+	os, err := ParseOS(input)
+	if err != nil {
+		panic(err)
+	}
+
+	return os
+}
+
+/* ---------------------- Impl: encoding.UnmarshalText ---------------------- */
+
+func (o *OS) UnmarshalText(bb []byte) error {
+	value, err := ParseOS(string(bb))
+	if err != nil {
+		return err
+	}
+
+	*o = value
+
+	return nil
+}
