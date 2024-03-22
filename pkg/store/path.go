e2cb02d07d0eee440ae1175c87a48ff89147f2be
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-03-22 12:46:29 -0700

    feat(store): implement a `gdbuild` store

---
+++ pkg/store/path.go
@@ -0,0 +1,57 @@
+package store
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/coffeebeats/gdbuild/internal/archive"
+	"github.com/coffeebeats/gdbuild/pkg/build"
+)
+
+const envStore = "GDBUILD_HOME"
+
+var (
+	ErrInvalidPath   = errors.New("invalid file path")
+	ErrMissingEnvVar = errors.New("missing environment variable")
+)
+
+/* -------------------------------------------------------------------------- */
+/*                          Function: TemplateArchive                         */
+/* -------------------------------------------------------------------------- */
+
+// TemplateArchive returns the full path (starting with the store path) to the
+// export template artifact archive within the store.
+//
+// NOTE: This does *not* mean the template archive exists.
+func TemplateArchive(storePath string, t build.Template) (string, error) {
+	if storePath == "" {
+		return "", ErrMissingStore
+	}
+
+	cs, err := t.Checksum()
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(storePath, storeDirTemplate, cs+archive.FileExtension), nil
+}
+
+/* -------------------------------------------------------------------------- */
+/*                               Function: Path                               */
+/* -------------------------------------------------------------------------- */
+
+// Returns the user-configured path to the 'gdbuild' store.
+func Path() (string, error) {
+	path := os.Getenv(envStore)
+	if path == "" {
+		return "", fmt.Errorf("%w: %s", ErrMissingEnvVar, envStore)
+	}
+
+	if !filepath.IsAbs(path) {
+		return "", fmt.Errorf("%w; expected absolute path: %s", ErrInvalidPath, path)
+	}
+
+	return filepath.Clean(path), nil
+}
