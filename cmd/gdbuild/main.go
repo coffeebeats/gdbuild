a79c8790c39ba272236568b49316953732fe37fc
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-04-05 14:56:32 -0700

    chore: release v0.3.0

--- cmd/gdbuild/main.go
+++ cmd/gdbuild/main.go
@@ -52,7 +52,7 @@ func main() { //nolint:funlen
 
 	app := &cli.App{
 		Name:    "gdbuild",
-		Version: "v0.2.5", // x-release-please-version
+		Version: "v0.3.0", // x-release-please-version
 
 		Suggest:                true,
 		UseShortOptionHandling: true,
